019041443ddec222721945599bb0534567c560ea
Carlos Pliego <carlos.oogs@gmail.com>
2018-01-22 13:18:47 -0700

    updated

--- configs/bin/init.sh
+++ configs/bin/init.sh
@@ -1,2 +1,2 @@
 # creates a make file for installation based on the one found in configs/make_templates/makefile
-source configs/env.sh; rm -rf makefile; envsubst < "configs/make_templates/makefile" > "makefile"
+source configs/env.sh; rm -rf makefile; envsubst < "configs/templates/make/makefile" > "makefile"
