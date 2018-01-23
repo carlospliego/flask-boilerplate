63bd01597e963cb4860b70e669078e2e5184308a
Carlos Pliego <carlos.oogs@gmail.com>
2018-01-23 16:42:44 -0700

    saved

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -6,9 +6,18 @@ from accounts.models import User;
 
 accounts_app = Blueprint('accounts_app', __name__)
 
-@accounts_app.route('/')
+@accounts_app.route('/users')
 def index():
     users = User.objects.all().exclude("id")
     resp = Response(users.to_json(), status=200, mimetype='application/json')
 
     return resp 
+
+@accounts_app.route('/signup', methods=['POST'])
+def signup():
+    
+    return "great"
+
+@accounts_app.route('/', methods=['GET'])
+def login():
+    return "greetings!"
