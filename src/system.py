d02fc667527a381920739c97c2ef2fb3e0be08a6
Carlos Pliego <carlos.oogs@gmail.com>
2018-01-26 22:08:44 -0700

    just saving state

--- src/system.py
+++ src/system.py
@@ -2,20 +2,36 @@ from flask import Flask
 from mongoengine import *
 from flask_jwt_extended import JWTManager
 from common.settings import SETTINGS
+from flask_restful import Resource, Api
 
 app = None
 
+api = None
+
+##todo PICKUP here https://flask-restful.readthedocs.io/en/latest/quickstart.html
 
 def create_app():
     global app
+    global api
 
     if not app:
         app = Flask(__name__)
+
+    if not api:
+        api = Api(app)
 
     from views.user import user
     app.register_blueprint(user, url_prefix='/user')
     from views.auth import auth
     app.register_blueprint(auth, url_prefix='/auth')
+
+
+    ### HERE
+    class HelloWorld(Resource):
+        def get(self):
+            return {'hello': 'world'}
+
+    api.add_resource(HelloWorld, '/')
 
     # load settings
     app.config = {**app.config, **SETTINGS}
