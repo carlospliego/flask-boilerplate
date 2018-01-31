0ba1744e90634302b2f64e7f9b0f88c5fb5e6f56
Carlos Pliego <carlos.oogs@gmail.com>
2018-01-30 22:58:02 -0700

    updated return functions

--- src/views/user.py
+++ src/views/user.py
@@ -1,94 +1,25 @@
 from flask import (
-    Blueprint,
-    Response,
-    request
+    Blueprint
 )
 from flask_jwt_extended import jwt_required
 from models.user import User
-from common.decorators import paginated
-
-import json
+from common.decorators import paginated, query, composed, json_res
 
 user = Blueprint('user', __name__)
 
+
 @user.route('/search', methods=['GET'])
-@jwt_required
-@paginated
-def query(pag):
-
-    _q = request.args.get('q')
-
-    ## make this a decorator
-    if not _q:
-        return Response(json.dumps({'msg': 'query string is required'}), 400, mimetype='application/json')
-
-    try:
-       raw =  json.loads(_q)
-    except ValueError:
-        return Response(json.dumps({'msg': 'invalid json'}), 400, mimetype='application/json')
-
+@composed(jwt_required, paginated, query, json_res)
+def query(pag, q):
     users = User.objects(
-        __raw__=raw
+        __raw__=q
     ).skip(pag['offset']).limit(pag['limit']).exclude("id", "password")
 
-    return Response(users.to_json(), status=200, mimetype='application/json')
-
-
+    return users.to_json(), 200
 
 
 @user.route('/', methods=['GET'])
-@jwt_required
-def index():
-
-
-    #
-    # # no pagination
-    users = User.objects.all().exclude("id", "password")
-    #
-    # # pagination
-    # users = User.objects(
-    #     __raw__=json.loads(q)
-    # ).skip(offset).limit(limit).exclude("id")
-    #
-    # # JSONDecodeError
-    # # BrokenFilesystemWarning
-
-
-
-    resp = Response(users.to_json(), status=200, mimetype='application/json')
-
-    return resp
-
-    # #--------------------------------------------------------------------------
-    #
-    # page_number = 1
-    # items_per_page = 10
-    # offset = (page_number - 1) * items_per_page
-    # q = request.args.get('q')
-    #
-    # # todo put json.loads in a try catch
-    # users = User.objects(
-    #     __raw__=json.loads(q)
-    # ).skip(offset).limit(items_per_page).exclude("id")
-    #
-    # # --------------------------------------------------------------------------
-
-    # request data
-#   page = int(request.args.get('page')) # try catch
-#    limit = int(request.args.get('limit')) #try catch
-#    offset = (page - 1) * limit
-
-# try:
-#     query_json = json.loads(request.args.get('q'))
-# except ValueError:
-
-
-# if q is not None:
-#     users = User.objects(
-#         __raw__=json.loads(q)
-#     ).exclude("id", "password")
-# else:
-#     users = User.objects.all().exclude("id")
-
-
-##
+@composed(jwt_required, paginated, json_res)
+def index(pag):
+    users = User.objects.all().skip(pag['offset']).limit(pag['limit']).exclude("id", "password")
+    return users.to_json(), 200
