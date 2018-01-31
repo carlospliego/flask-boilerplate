5968148e202b63209397f799b7eac19d265eb2d2
Carlos Pliego <carlos.oogs@gmail.com>
2018-01-30 23:01:09 -0700

    updated return functions

--- src/common/decorators.py
+++ src/common/decorators.py
@@ -1,6 +1,13 @@
 from functools import wraps
-from flask import g, request, jsonify, Response
+from flask import g, request, jsonify, make_response, Response
+import json
 
+def composed(*decs):
+    def deco(f):
+        for dec in reversed(decs):
+            f = dec(f)
+        return f
+    return deco
 
 def json_only(f):
 
@@ -28,8 +35,52 @@ def paginated(f):
             page = int(_default_page if not _page else _page)
             limit = int(_default_items if not _limit else _limit)
             offset = (page - 1) * limit
-        except ValueError:
-            return Response(jsonify({'msg': 'could not parse page and limit'}), 400, mimetype='application/json')
 
-        return f({'offset': offset, 'limit': limit})
+            # todo add something a little more definative rather than 999999999
+            if offset > 999999999:
+                return jsonify({'msg': 'paging input is too large'}), 400,
+
+        except ValueError:
+            return jsonify({'msg': 'could not parse page and limit'}), 400
+
+        return f(*args, {'offset': offset, 'limit': limit})
     return decorator
+
+
+def query(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        _q = request.args.get('q')
+
+        if not _q:
+            return jsonify({'msg': 'query string is required'}), 400
+        try:
+            raw = json.loads(_q)
+        except ValueError:
+            return jsonify({'msg': 'invalid json'}), 400
+
+        return f(*args, raw)
+    return decorator
+
+
+def add_response_headers(headers={}):
+    """This decorator adds the headers passed in to the response"""
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            resp = make_response(f(*args, **kwargs))
+            h = resp.headers
+            for header, value in headers.items():
+                h[header] = value
+            return resp
+        return decorated_function
+    return decorator
+
+
+def json_res(f):
+    @wraps(f)
+    @add_response_headers({'Content-Type':'application/json'})
+    def decorated(*args, **kwargs):
+        return f(*args, **kwargs)
+    return decorated
+
