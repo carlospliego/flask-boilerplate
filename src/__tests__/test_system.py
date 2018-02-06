a1dcd17ab338a6d8ee2cbdd5692ef21cf84d1543
Carlos Pliego <carlos.oogs@gmail.com>
2018-02-06 15:55:36 -0700

    added system tests

--- src/__tests__/test_system.py
+++ src/__tests__/test_system.py
@@ -1,15 +1,28 @@
 import unittest
- 
-class TestUM(unittest.TestCase):
+from system import create_app, connect_db
+from pprint import pprint
+from pymongo import MongoClient
+
+class TestSystem(unittest.TestCase):
  
     def setUp(self):
+        self.app = create_app()
         pass
  
-    def test_numbers_3_4(self):
-        self.assertEqual( 1, 1)
- 
-    def test_strings_a_3(self):
-        self.assertEqual( 2,2)
- 
+    def test_create_app(self):
+        self.assertIsNotNone(self.app)
+
+    def test_added_blueprints(self):
+        self.assertEqual(list(self.app.blueprints.keys()), ['user', 'auth'])
+
+    def test_config(self):
+        self.assertEqual(self.app.config['JWT_SECRET_KEY'], 'super-secret')
+        self.assertEqual(self.app.config['HOST'], '0.0.0.0')
+
+    def test_connect_db(self):
+        db = connect_db('flask', 'mongodb')
+        self.assertTrue(isinstance(db, MongoClient))
+
+
 if __name__ == '__main__':
     unittest.main()
