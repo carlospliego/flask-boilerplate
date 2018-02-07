43fdad153403707d1036d8040c1c92eaaf4b03cc
Carlos Pliego <carlos.oogs@gmail.com>
2018-02-07 11:26:12 -0700

    added some vs code debug settings

--- src/__tests__/test_system.py
+++ src/__tests__/test_system.py
@@ -7,17 +7,12 @@ class TestSystem(unittest.TestCase):
  
     def setUp(self):
         self.app = create_app()
-        pass
  
     def test_create_app(self):
         self.assertIsNotNone(self.app)
 
     def test_added_blueprints(self):
         self.assertEqual(list(self.app.blueprints.keys()), ['user', 'auth'])
-
-    def test_config(self):
-        self.assertEqual(self.app.config['JWT_SECRET_KEY'], 'super-secret')
-        self.assertEqual(self.app.config['HOST'], '0.0.0.0')
 
     def test_connect_db(self):
         db = connect_db('flask', 'mongodb')
